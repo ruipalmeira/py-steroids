4a3e53142275d7099bfc54e46a45818130aa3c54
Rui Palmeira <ruipalmeira@gmail.com>
2025-11-04 10:48:54 +0000

    added 60fps lock to the gameloop

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameclock = pygame.time.Clock()
+    dt = 0
+        
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     #create infinite game loop
     while True:
         for event in pygame.event.get():
@@ -11,10 +18,9 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        gameclock.tick(60)
+        dt = (gameclock.tick(60)/1000)
+        print(f"secconds: {dt}")
 
 if __name__ == "__main__":
     main()
